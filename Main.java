a2d25c3fd4b7f02657660b6319e5df31fd0abcdb
root <root@ip-172-31-8-226.ap-south-1.compute.internal>
2024-11-22 05:59:45 +0000

    New version

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-      System.out.println("FA21-BCS-013");
+      System.out.println("FA21-BCS-013 second version!!");
   }
 }
